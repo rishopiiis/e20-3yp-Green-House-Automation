e34f98e9dc6e03cd9fb70c38ee2be398a6c31309
Vithustennysan8 <e20416@eng.pdn.ac.lk>
2025-02-14 11:20:51 +0530

    Green_Tech files added

---
+++ code/BackEnd_MobileApp/Green_Tech/src/main/java/com/Green_Tech/Green_Tech/GreenTechApplication.java
@@ -0,0 +1,13 @@
+package com.Green_Tech.Green_Tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenTechApplication.class, args);
+	}
+
+}
---
+++ code/BackEnd_MobileApp/Green_Tech/src/test/java/com/Green_Tech/Green_Tech/GreenTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Green_Tech.Green_Tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

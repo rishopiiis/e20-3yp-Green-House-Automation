0c376031d5e50b2ff402f3a60c93c2ab5177e57d
rishopiiis <e20371@eng.pdn.ac.lk>
2025-02-20 12:23:03 +0530

    newly  added

--- code/FrontEnd_MobileApp/App.tsx
+++ code/FrontEnd_MobileApp/App.tsx
@@ -1,24 +1,28 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { useState } from 'react';
-import Page from './app/index';
-import login from './app/Components/Authentication/login';
-import register from './app/Components/Authentication/register';
-import about from './app/about';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import { useState } from "react";
+import login from "./app/Components/Authentication/login";
+import register from "./app/Components/Authentication/register";
+import about from "./app/about";
+import Page from "./app/index";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const [isLogin, setisLogin] = useState(false);
-  
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={isLogin? "page": "login"}>
-        <Stack.Screen name="login" component={login} />
-        <Stack.Screen name="about" component={about} />
-        <Stack.Screen name="page" component={Page} />
-        <Stack.Screen name="register" component={register} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={isLogin ? "page" : "login"}>
+          <Stack.Screen name="login" component={login} />
+          <Stack.Screen name="about" component={about} />
+          <Stack.Screen name="page" component={Page} />
+          <Stack.Screen name="register" component={register} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
--- code/FrontEnd_MobileApp/app/Components/Manual/SlideNew.tsx
+++ code/FrontEnd_MobileApp/app/Components/Manual/SlideNew.tsx
@@ -1,7 +1,232 @@
+// import React, { useState } from "react";
+
+// // import { Animated } from "react-native";
+
+// import {
+//   Animated,
+//   Dimensions,
+//   Image,
+//   ScrollView,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   View,
+// } from "react-native";
+// import Footer from "../common/Footer";
+// import Header from "../common/Header";
+
+// const { width } = Dimensions.get("window");
+// const AnimatedText = Animated.createAnimatedComponent(Text);
+
+// const imageData = [
+//   {
+//     id: 1,
+//     name: "ESP32",
+//     image: require("../../../assets/ESP32.jpeg"),
+//     description:
+//       "The ESP32 is a low-cost, low-power system on a chip (SoC) that features Wi-Fi and Bluetooth capabilities. It is widely used in Internet of Things (IoT) projects and can connect sensors to the cloud or mobile apps. The ESP32 allows easy integration of various sensors and is perfect for wireless communication.",
+//   },
+//   {
+//     id: 2,
+//     name: "DHT22",
+//     image: require("../../../assets/DHT22.jpeg"),
+//     description:
+//       "The DHT22 (also known as AM2302) is a digital sensor used to measure temperature and humidity. It provides reliable data with a wide range of measurements (temperature: -40°C to 80°C, humidity: 0-100%). It is ideal for applications requiring environmental monitoring, like weather stations and HVAC systems.",
+//   },
+//   {
+//     id: 3,
+//     name: "Capasitive moisture sensor",
+//     image: require("../../../assets/Moisture_Sensor.jpeg"),
+//     description:
+//       "The VH400 is a soil moisture sensor designed to measure the water content in the soil. It provides real-time data about soil moisture levels and is widely used in agriculture and gardening applications to ensure optimal watering and irrigation systems.",
+//   },
+//   {
+//     id: 4,
+//     name: "NPK sensor",
+//     image: require("../../../assets/NPK.jpeg"),
+//     description:
+//       "The NPK sensor measures the nitrogen (N), phosphorus (P), and potassium (K) content in the soil, which are essential nutrients for plant growth. This sensor helps determine the fertility of the soil and provides crucial data for optimizing fertilizers and improving plant health.",
+//   },
+// ];
+
+// const App = () => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const scaleAnim = new Animated.Value(1);
+
+//   const animatePress = () => {
+//     Animated.sequence([
+//       Animated.timing(scaleAnim, {
+//         toValue: 0.85,
+//         duration: 100,
+//         useNativeDriver: true,
+//       }),
+//       Animated.timing(scaleAnim, {
+//         toValue: 1,
+//         duration: 100,
+//         useNativeDriver: true,
+//       }),
+//     ]).start();
+//   };
+
+//   const goToNext = () => {
+//     if (currentIndex < imageData.length - 1) {
+//       animatePress();
+//       setCurrentIndex(currentIndex + 1);
+//     }
+//   };
+
+//   const goToPrevious = () => {
+//     if (currentIndex > 0) {
+//       animatePress();
+//       setCurrentIndex(currentIndex - 1);
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Header
+//         viewZone={false}
+//         selectedZone={""}
+//         setSelectedZone={(zone: string) => {}}
+//       />
+
+//       <Text style={styles.heading}>{imageData[currentIndex].name}</Text>
+//       <ScrollView contentContainerStyle={styles.scrollContent}>
+//         {/* Image Display */}
+//         <View style={styles.imageContainer}>
+//           <Image source={imageData[currentIndex].image} style={styles.image} />
+//         </View>
+
+//         <Text style={styles.description}>
+//           {imageData[currentIndex].description}
+//         </Text>
+
+//         {/* Navigation Controls */}
+//         <View style={styles.navigationContainer}>
+//           {/* Left Arrow */}
+//           <TouchableOpacity
+//             onPress={goToPrevious}
+//             style={[
+//               styles.arrowButton,
+//               currentIndex === 0 ? styles.disabledButton : styles.activeButton,
+//             ]}
+//             disabled={currentIndex === 0}
+//           >
+//             <Animated.Text style={styles.arrowText}>{"<"}</Animated.Text>
+//           </TouchableOpacity>
+
+//           {/* Page Number */}
+//           <View style={styles.pageNumberContainer}>
+//             <Text style={styles.pageText}>
+//               {currentIndex + 1} / {imageData.length}
+//             </Text>
+//           </View>
+
+//           {/* Right Arrow */}
+//           <TouchableOpacity
+//             onPress={goToNext}
+//             style={[
+//               styles.arrowButton,
+//               currentIndex === imageData.length - 1
+//                 ? styles.disabledButton
+//                 : styles.activeButton,
+//             ]}
+//             disabled={currentIndex === imageData.length - 1}
+//           >
+//             <Animated.Text style={styles.arrowText}>{">"}</Animated.Text>
+//           </TouchableOpacity>
+//         </View>
+//       </ScrollView>
+
+//       <Footer />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "rgb(4,38,28)",
+//   },
+//   scrollContent: {
+//     flexGrow: 1,
+//     alignItems: "center",
+//     marginTop: 30,
+//   },
+//   heading: {
+//     color: "#F6FCDF",
+//     fontSize: 36,
+//     textAlign: "center",
+//     marginTop: "20%",
+//     fontWeight: "bold",
+//   },
+//   imageContainer: {
+//     width: "85%",
+//     maxWidth: 420,
+//     height: 300,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     marginBottom: 20,
+//     borderRadius: 10,
+//     borderWidth: 4,
+//     borderColor: "#f1f5f9",
+//     overflow: "hidden",
+//   },
+//   image: {
+//     width: "100%",
+//     height: "100%",
+//     resizeMode: "cover",
+//   },
+//   description: {
+//     fontSize: 22,
+//     width: "85%",
+//     textAlign: "justify",
+//     color: "#F6FCDF",
+//     marginBottom: 20,
+//   },
+//   navigationContainer: {
+//     display: "flex",
+//     flexDirection: "row",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginTop: 10,
+//     marginBottom: "20%",
+//   },
+//   arrowButton: {
+//     paddingVertical: 10,
+//     paddingHorizontal: 20,
+//     borderRadius: 8,
+//     elevation: 5,
+//     borderWidth: 0.6,
+//     borderColor: "rgb(1,105,77)",
+//   },
+//   activeButton: {
+//     backgroundColor: "rgb(1,105,77)",
+//   },
+//   disabledButton: {
+//     backgroundColor: "rgba(1, 105, 77, 0.34)",
+//   },
+//   pageNumberContainer: {
+//     paddingVertical: 8,
+//     paddingHorizontal: 15,
+//     borderRadius: 8,
+//     marginHorizontal: 10,
+//   },
+//   pageText: {
+//     fontSize: 20,
+//     color: "white",
+//     fontWeight: "bold",
+//   },
+//   arrowText: {
+//     fontSize: 30,
+//     color: "white",
+//     fontWeight: "bold",
+//   },
+// });
+
+// export default App;
+
 import React, { useState } from "react";
-
-// import { Animated } from "react-native";
-
 import {
   Animated,
   Dimensions,
@@ -12,6 +237,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // Import GestureHandlerRootView
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 
@@ -35,7 +261,7 @@ const imageData = [
   },
   {
     id: 3,
-    name: "Capasitive moisture sensor",
+    name: "Capacitive moisture sensor",
     image: require("../../../assets/Moisture_Sensor.jpeg"),
     description:
       "The VH400 is a soil moisture sensor designed to measure the water content in the soil. It provides real-time data about soil moisture levels and is widely used in agriculture and gardening applications to ensure optimal watering and irrigation systems.",
@@ -83,63 +309,71 @@ const App = () => {
   };
 
   return (
-    <View style={styles.container}>
-      <Header
-        viewZone={false}
-        selectedZone={""}
-        setSelectedZone={(zone: string) => {}}
-      />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      {/* Wrap with GestureHandlerRootView */}
+      <View style={styles.container}>
+        <Header
+          viewZone={false}
+          selectedZone={""}
+          setSelectedZone={(zone: string) => {}}
+        />
 
-      <Text style={styles.heading}>{imageData[currentIndex].name}</Text>
-      <ScrollView contentContainerStyle={styles.scrollContent}>
-        {/* Image Display */}
-        <View style={styles.imageContainer}>
-          <Image source={imageData[currentIndex].image} style={styles.image} />
-        </View>
-
-        <Text style={styles.description}>
-          {imageData[currentIndex].description}
-        </Text>
-
-        {/* Navigation Controls */}
-        <View style={styles.navigationContainer}>
-          {/* Left Arrow */}
-          <TouchableOpacity
-            onPress={goToPrevious}
-            style={[
-              styles.arrowButton,
-              currentIndex === 0 ? styles.disabledButton : styles.activeButton,
-            ]}
-            disabled={currentIndex === 0}
-          >
-            <Animated.Text style={styles.arrowText}>{"<"}</Animated.Text>
-          </TouchableOpacity>
-
-          {/* Page Number */}
-          <View style={styles.pageNumberContainer}>
-            <Text style={styles.pageText}>
-              {currentIndex + 1} / {imageData.length}
-            </Text>
+        <Text style={styles.heading}>{imageData[currentIndex].name}</Text>
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+          {/* Image Display */}
+          <View style={styles.imageContainer}>
+            <Image
+              source={imageData[currentIndex].image}
+              style={styles.image}
+            />
           </View>
 
-          {/* Right Arrow */}
-          <TouchableOpacity
-            onPress={goToNext}
-            style={[
-              styles.arrowButton,
-              currentIndex === imageData.length - 1
-                ? styles.disabledButton
-                : styles.activeButton,
-            ]}
-            disabled={currentIndex === imageData.length - 1}
-          >
-            <Animated.Text style={styles.arrowText}>{">"}</Animated.Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+          <Text style={styles.description}>
+            {imageData[currentIndex].description}
+          </Text>
 
-      <Footer />
-    </View>
+          {/* Navigation Controls */}
+          <View style={styles.navigationContainer}>
+            {/* Left Arrow */}
+            <TouchableOpacity
+              onPress={goToPrevious}
+              style={[
+                styles.arrowButton,
+                currentIndex === 0
+                  ? styles.disabledButton
+                  : styles.activeButton,
+              ]}
+              disabled={currentIndex === 0}
+            >
+              <Animated.Text style={styles.arrowText}>{"<"}</Animated.Text>
+            </TouchableOpacity>
+
+            {/* Page Number */}
+            <View style={styles.pageNumberContainer}>
+              <Text style={styles.pageText}>
+                {currentIndex + 1} / {imageData.length}
+              </Text>
+            </View>
+
+            {/* Right Arrow */}
+            <TouchableOpacity
+              onPress={goToNext}
+              style={[
+                styles.arrowButton,
+                currentIndex === imageData.length - 1
+                  ? styles.disabledButton
+                  : styles.activeButton,
+              ]}
+              disabled={currentIndex === imageData.length - 1}
+            >
+              <Animated.Text style={styles.arrowText}>{">"}</Animated.Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+
+        <Footer />
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
